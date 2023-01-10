5a7865535948c20d463b7cf3025dac1e29d75cd7
UngSyMui <ungsymui@gmail.com>
2023-01-10 18:47:44 +0700

    Bai Thi

---
+++ ConsoleApp1/BaiThi/Animal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.BaiThi
+{
+    public class Animal
+    {
+        public int weight { get; set; }
+        public string name { get; set; }
+        public Animal(int weight,string name)
+        {
+            this.weight = weight;
+            this.name = name;
+        }
+        public void Show()
+        {
+            Console.WriteLine("Name : " + this.name + "Weight : " + this.weight);
+        }
+        public void SetMe(int weight,string name)
+        {
+            this.weight=weight;
+            this.name = name;
+        }
+
+    }
+}
